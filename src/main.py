90eaad0892d04da3d3d9e9a105ec5967f3f3abb3
Nayeon Shin <contact@nayeonshin.dev>
2022-05-23 13:34:12 +0900

    chore(`main.py`): comment in commented out blocks

--- src/main.py
+++ src/main.py
@@ -11,52 +11,52 @@ from items import gifts_from_friend
 from cleaning_desk import TrashCan
 from washing_clothes import WashingMachine
 
-# # Part 1. Unpacking
-# # You've got a package from a friend that consists of 3 items.
-# # Try unboxing and inspecting it.
-# # YOUR CODE GOES HERE ---
-# item1, item2, item3 = gifts_from_friend
-# inspect(item1, item2, item3)
-# # -----------------------
+# Part 1. Unpacking
+# You've got a package from a friend that consists of 3 items.
+# Try unboxing and inspecting it.
+# YOUR CODE GOES HERE ---
+item1, item2, item3 = gifts_from_friend
+inspect(item1, item2, item3)
+# -----------------------
 
-# # The second item turned out to be a bouquet.
-# # You want to check what flowers there are
-# # and what each of them smells like.
-# # Can you take out and smell each flower from the bouquet?
-# # YOUR CODE GOES HERE ---
-# freesia, rose, anemone, lily_of_the_valley = item2
+# The second item turned out to be a bouquet.
+# You want to check what flowers there are
+# and what each of them smells like.
+# Can you take out and smell each flower from the bouquet?
+# YOUR CODE GOES HERE ---
+freesia, rose, anemone, lily_of_the_valley = item2
 
-# smell(freesia)
-# smell(rose)
-# smell(anemone)
-# smell(lily_of_the_valley)
-# # -----------------------
+smell(freesia)
+smell(rose)
+smell(anemone)
+smell(lily_of_the_valley)
+# -----------------------
 
 
-# # # Part 2. Unpacking & *args
-# # Oh no! Your desk is a mess.
-# # Throw away all the trash in a can and empty the can.
-# items_on_desk = [
-#     "pencil case",
-#     "empty water bottle",
-#     "eraser crumbs",
-#     "used tissue",
-#     "battery charger",
-# ]
+# # Part 2. Unpacking & *args
+# Oh no! Your desk is a mess.
+# Throw away all the trash in a can and empty the can.
+items_on_desk = [
+    "pencil case",
+    "empty water bottle",
+    "eraser crumbs",
+    "used tissue",
+    "battery charger",
+]
 
-# trash_can = TrashCan()
+trash_can = TrashCan()
 
-# # YOUR CODE GOES HERE ---
-# _, *trash, _ = items_on_desk
+# YOUR CODE GOES HERE ---
+_, *trash, _ = items_on_desk
 
-# trash_can.check_status()
+trash_can.check_status()
 
-# trash_can.put(*trash)
-# trash_can.check_status()
+trash_can.put(*trash)
+trash_can.check_status()
 
-# trash_can.empty()
-# trash_can.check_status()
-# # -----------------------
+trash_can.empty()
+trash_can.check_status()
+# -----------------------
 
 
 # Part 3. Unpacking & **kwargs
