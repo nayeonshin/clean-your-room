138f6e53f89b1ffa7abaabfd093377581af8f8c6
Nayeon Shin <contact@nayeonshin.dev>
2022-05-23 12:18:48 +0900

    feat: update `start()` in `class WashingMachine`

--- src/washing_clothes.py
+++ src/washing_clothes.py
@@ -2,19 +2,24 @@ class WashingMachine:
     def __init__(self):
         self.detergent = None
         self.clothes = {}
+        self.is_clean = False
 
     def put(self, detergent, **kwargs):
         self.detergent = detergent
         self.clothes.update(kwargs)
 
     def start(self):
-        pass
+        print()
+        print("Washing clothes... 🧼")
+        self.is_clean = True
 
     def check_status(self):
         print()
         print("🧺")
 
         if self.clothes:
+            status = "✨ CLEAN ✨" if self.is_clean else "💩 DIRTY 💩"
+            print(status)
             for cycle, clothes in self.clothes.items():
                 print(f"{cycle}:", clothes)
         else:
